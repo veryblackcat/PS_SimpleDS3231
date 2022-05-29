490c9a4b7bb13516d57efc60ec7c18e6f49c64e1
Piotr Sylwesiuk <47433781+veryblackcat@users.noreply.github.com>
2022-05-29 22:30:17 +0200

    in construction

--- src/psSimpleDS3231.cpp
+++ src/psSimpleDS3231.cpp
@@ -5,6 +5,30 @@
 #include <Wire.h>
 #include <psSimpleDS3231.h>
 
-rtcDS3231::rtcDS3231() {
-	
+rtcDS3231::rtcDS3231(uint8_t addr) {
+//rtcDS3231::rtcDS3231(TwoWire &twi, uint8_t addr) {
+	//rtcDS3231::twi = &twi;
+    rtcDS3231::twi = &Wire;
+    addressRTC = addr;
+}
+
+void rtcDS3231::begin() {
+    twi->begin();
+}
+
+void rtcDS3231::test() {
+    twi->beginTransmission(DS3231_ADDRESS);
+    twi->write(0);
+    twi->endTransmission();
+
+    uint8_t b = twi->requestFrom(DS3231_ADDRESS, 19); 
+    uint8_t i = 0;
+    while (twi->available()) {
+        t[i] = twi->read();
+        i++;
+  }
+}
+
+uint8_t rtcDS3231::read(uint8_t i) {
+    return(t[i]);
 }
--- src/psSimpleDS3231.h
+++ src/psSimpleDS3231.h
@@ -7,11 +7,21 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+#define DS3231_ADDRESS 0x68 
+
 class rtcDS3231 {
 	public:
-		rtcDS3231(TwoWire &twi);
+		TwoWire *twi;
+		//rtcDS3231(TwoWire &twi, uint8_t addr=DS3231_ADDRESS);
+		rtcDS3231(uint8_t addr=DS3231_ADDRESS);
+		void begin();
+
+		uint8_t t[19];
+		void test();
+		uint8_t read(uint8_t i);
 		
 	protected:
-}
+		uint8_t addressRTC;
+};
 
 #endif // PS_SIMPLE_DS3231_H
