f3be345e603b8fa4045163246adb212407ef3e82
Piotr Sylwesiuk <47433781+veryblackcat@users.noreply.github.com>
2022-05-31 21:20:48 +0200

    updating

--- src/psSimpleDS3231.cpp
+++ src/psSimpleDS3231.cpp
@@ -31,39 +31,50 @@ void rtcDS3231::getTemperature() {
 void rtcDS3231::getControl() {
 
 }
-
 void rtcDS3231::setDateTime(uint8_t hour, uint8_t minutes, uint8_t seconds, uint8_t day, uint8_t date, uint8_t month, uint16_t year){
 
 }
-void rtcDS3231::hour(uint8_t hour) {
-    hour = bin2bcd(hour);   //mode 24h (BIT6 = 0)
-    
+void rtcDS3231::seconds(uint8_t seconds) {
+    seconds = bin2bcd(seconds);
+    writeByte(0x00, seconds);
 }
 void rtcDS3231::minutes(uint8_t minutes) {
     minutes = bin2bcd(minutes);
-    
+    writeByte(0x01, minutes);
 }
-void rtcDS3231::seconds(uint8_t seconds) {
-    seconds = bin2bcd(seconds);
-    
+void rtcDS3231::hour(uint8_t hour) {
+    hour = bin2bcd(hour);   //mode 24h (BIT6 = 0)
+    writeByte(0x02, hour);
 }
 void rtcDS3231::dayOfWeek(uint8_t dow) {
-    
+    writeByte(0x03, dow);
 }
 void rtcDS3231::day(uint8_t day) {
     day = bin2bcd(day);
-    
+    writeByte(0x04, day);
 }
 void rtcDS3231::month(uint8_t month) {
-    month = bin2bcd(month) | (dataBuffer[5] & 0x80);
-    
+    month = bin2bcd(month) | (dataBuffer[5] & 0x80); // includes century
+    writeByte(0x05, month);
 }
+/*
 void rtcDS3231::year(uint16_t year) {
-    uint8_t _century = dataBuffer[5] & 0x80;
+    uint8_t _century = dataBuffer[5] & 0x80; // includes century
     if (year < 2100) _century = 0x80;
     else _century = 0x00;
     _century |= dataBuffer[5];
     year = bin2bcd(year%100);
+    writeByte(0x05, _century);
+    writeByte(0x06, year);
+}*/
+void rtcDS3231::year(uint16_t year) {
+    uint8_t _year[2];
+    _year[0] = dataBuffer[5] & 0x80; // includes century
+    if (year < 2100) _year[0] = 0x80;
+    else _year[0] = 0x00;
+    _year[0] |= dataBuffer[5];
+    _year[1] = bin2bcd(year%100);
+    writeBytes(_year, 2, 0x05);
 }
 uint8_t rtcDS3231::readBytes(uint8_t startingPointer, uint8_t nrBytes) {
     twi->beginTransmission(addressRTC);
@@ -77,4 +88,16 @@ uint8_t rtcDS3231::readBytes(uint8_t startingPointer, uint8_t nrBytes) {
         i++;
   }
   return(_nr);
+}
+void rtcDS3231::writeByte(uint8_t startingPointer, uint8_t data) {
+    twi->beginTransmission(addressRTC);
+    twi->write(startingPointer);
+    twi->write(data);
+    twi->endTransmission();
+}
+void rtcDS3231::writeBytes(uint8_t data[], uint8_t length, uint8_t startingPointer) {
+    twi->beginTransmission(addressRTC);
+    twi->write(startingPointer);
+    twi->write(data, length);
+    twi->endTransmission();
 }
--- src/psSimpleDS3231.h
+++ src/psSimpleDS3231.h
@@ -45,6 +45,9 @@ class rtcDS3231 {
 		void year(uint16_t year);
 
 		uint8_t readBytes(uint8_t startingPointer, uint8_t nrBytes);
+		void writeByte(uint8_t startingPointer, uint8_t data);
+		void writeBytes(uint8_t data[], uint8_t length, uint8_t startingPointer);
+
 		uint8_t bcd2bin (uint8_t bcd) { return((bcd & 0x0f) + (bcd >> 4) * 10); }
 		uint8_t bin2bcd (uint8_t bin) { return((bin/10)<<4 + bin%10); } // input range: 0 - 99
 		
