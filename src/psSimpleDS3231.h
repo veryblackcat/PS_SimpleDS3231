daa0936e08ff520ac8d2160b97d831eeb57b9116
Piotr Sylwesiuk <47433781+veryblackcat@users.noreply.github.com>
2022-06-02 09:05:56 +0200

    updating

--- src/psSimpleDS3231.h
+++ src/psSimpleDS3231.h
@@ -42,7 +42,7 @@ class rtcDS3231 {
 		void minutes(uint8_t minutes) { writeByte(0x01, bin2bcd(minutes)); }
 		void hour(uint8_t hour) { writeByte(0x02, bin2bcd(hour)); }  //mode 24h (BIT6 = 0)
 		void dayOfWeek(uint8_t dow) { writeByte(0x03, dow); }
-		void day(uint8_t day) { writeByte(0x04, bin2bcd(day)); }
+		void day(uint8_t day) { writeByte(0x04, bin2bcd(day)); } // For DS3231 - 1 equals Sunday, then 2 equals Monday, and so on.
 		void month(uint8_t month) { writeByte(0x05, (bin2bcd(month) | (dataBuffer[5] & 0x80))); } // includes century
 		void year(uint16_t year);
 
