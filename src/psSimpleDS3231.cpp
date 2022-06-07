f904dc0be43eba58016936b1377f96c496dcad2f
Piotr Sylwesiuk <47433781+veryblackcat@users.noreply.github.com>
2022-06-07 06:36:04 +0200

    calculateDayOfWeek

--- src/psSimpleDS3231.cpp
+++ src/psSimpleDS3231.cpp
@@ -31,11 +31,12 @@ void rtcDS3231::getTemperature() {
 
 void rtcDS3231::setDateTime(uint8_t hour, uint8_t minutes, uint8_t seconds, uint8_t day, uint8_t month, uint16_t year) {
     // dow liczony automatycznie
+    // The day of the week for DS3231 - 1 equals Sunday, then 2 equals Monday, and so on.
     uint8_t _dt[7];
     _dt[0] = bin2bcd(seconds);
     _dt[1] = bin2bcd(minutes);
     _dt[2] = bin2bcd(hour);
-    _dt[3] = calculateDayOfWeek(day, month, year);
+    _dt[3] = calculateDayOfWeek(day, month, year) + 1;
     _dt[4] = bin2bcd(day);
     _dt[5] = (year < 2100) ? bin2bcd(month) : (bin2bcd(month) & 0x80);
     _dt[6] = bin2bcd(year % 100);
@@ -50,8 +51,9 @@ void rtcDS3231::setTime(uint8_t hour, uint8_t minutes, uint8_t seconds) {
 }
 void rtcDS3231::setDate(uint8_t day, uint8_t month, uint16_t year) {
     // dow liczony automatycznie
+    // The day of the week for DS3231 - 1 equals Sunday, then 2 equals Monday, and so on.
     uint8_t _date[4];
-    _date[0] = calculateDayOfWeek(day, month, year);
+    _date[0] = calculateDayOfWeek(day, month, year) + 1;
     _date[1] = bin2bcd(day);
     _date[2] = (year < 2100) ? bin2bcd(month) : (bin2bcd(month) & 0x80); // includes century
     _date[3] = bin2bcd(year % 100);
@@ -131,9 +133,8 @@ uint8_t rtcDS3231::calculateDayOfWeek(uint8_t day, uint8_t month, uint16_t year)
     /* split by century */
     cent = year / 100;
     year %= 100;
-    //return (((26 * month - 2) / 10 + day + year + year / 4 + cent / 4 + 5 * cent) % 7);
-    // For DS3231 - 1 equals Sunday, then 2 equals Monday, and so on.
-    return ((((26 * month - 2) / 10 + day + year + year / 4 + cent / 4 + 5 * cent) % 7) + 1);
+    // 0 equals Sunday, then 1 equals Monday, and so on.
+    return (((26 * month - 2) / 10 + day + year + year / 4 + cent / 4 + 5 * cent) % 7);
 }
 void rtcDS3231::calculateSummerWinterDay() {
     summerTimeDay = 31 - calculateDayOfWeek(31, 03, YYYY) + 1; // The last Sunday of March.
